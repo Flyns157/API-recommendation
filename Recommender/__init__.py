43359432fc31e9664d6b4086b97f70c04689c694
Mattéo Cuisset <matteo.cuisset@gmail.com>
2024-12-11 11:37:24 +0100

    Refactor API to use FastAPI; updated version to 0.2.5. Replacing Flask with FastAPI framework, reorganized logging and middleware setup, and modified route management. Removed legacy class and methods.

--- Recommender/__init__.py
+++ Recommender/__init__.py
@@ -1,118 +1,70 @@
 """
-This module defines the RecommendationAPI class, which extends Flask to create a customizable API server
-for recommendation services. It includes authentication, rate limiting, logging, and configurable modes
-(deploy, debug, maintenance) for different use cases.
+__init__.py
 
-Modules:
-    - flask_limiter.util: Provides utilities for rate-limiting based on the client's remote address.
-    - flask_jwt_extended: Manages JWT authentication for the API.
-    - Database, Utils: Utility modules for database interactions and general helper functions.
-    - flask_limiter: Enables rate limiting for API endpoints.
-    - Config: Configuration settings for the API.
-    - logging: Used to set up a logging system with both file and stream handlers.
-
-Classes:
-    - RecommendationAPI: The main API server class, handling initialization and configuration for the API.
+This file is the entry point of the API. It initializes the FastAPI app and registers the routers.
 """
-__version__ = "0.1.5"
+__version__ = "0.2.5"
 
-from flask_limiter.util import get_remote_address
-from flask_jwt_extended import JWTManager
-from .database import Database
-from flask_limiter import Limiter
-from flask import Flask, jsonify
-from .utils.config import Config
+from fastapi import FastAPI, Request
+
 import logging
+import time
 
+# Initialize logging
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    handlers=[logging.FileHandler('recommender.log'), logging.StreamHandler()])
-logger = logging.getLogger(__name__)
-db = Database()
+                    handlers=[
+                        logging.FileHandler("app.log"),  # Log to a file
+                        logging.StreamHandler()          # Also log to console
+                    ])
+main_logger = logging.getLogger(__name__)
 
-class RecommendationAPI(Flask):
-    """
-    RecommendationAPI extends Flask to provide a recommendation server with JWT-based authentication,
-    rate limiting, logging, and configurable run modes. It sets up database, JWT, and rate-limiting 
-    integrations and registers API blueprints for authentication and recommendation.
 
-    Attributes:
-        limiter (Limiter): The rate-limiting manager, limiting the number of requests per day and hour.
-        db (Database): The database instance used for managing user and recommendation data.
-        logger (logging.Logger): The application logger.
-        jwt (JWTManager): The JSON Web Token manager for handling user authentication.
-        mode (str): The operational mode of the server, affecting availability and functionality.
-    """
-    
-    def __init__(self, *args, **kwargs):
-        """
-        Initialize the RecommendationAPI instance with Flask configuration, rate limiting, JWT,
-        logging, and route registration.
+class RecommenderFastAPI(FastAPI):
+    def __init__(self, title: str = "Recommender-FastAPI", version: str = __version__, *args, **kwargs):
+        super().__init__(title=title, version=version, *args, **kwargs)
+        self.setup()
 
-        Parameters:
-            *args: Variable length argument list passed to the Flask constructor.
-            **kwargs: Arbitrary keyword arguments passed to the Flask constructor.
-        """
-        super().__init__(*args, **kwargs)
-        self.config.from_object(Config)
-        self.limiter = Limiter(get_remote_address, default_limits=["200 per day", "50 per hour"])
-        self.db = db
-        self.logger = logging.getLogger(__name__)
-        self.jwt = JWTManager()
-        self.mode = 'maintenance'
-        
+    def setup(self) -> None:
+        self.setup_logging()
+        self.setup_middleware()
+        self.setup_routes()
+
+    def setup_logging(self) -> None:
+        self.logger = main_logger
+
+    def setup_middleware(self) -> None:
+        @self.middleware("http")
+        async def add_process_time_header(request: Request, call_next):
+            start_time = time.perf_counter()
+            self.logger.info(f"Request: {request.method} {request.url}")  # Log the request method and URL
+            response = await call_next(request)
+            process_time = time.perf_counter() - start_time
+            response.headers["X-Process-Time"] = str(process_time)
+            self.logger.info(f"Processed {request.method} {request.url} in {process_time:.4f} secs")  # Log the processing time
+            return response
+
+    def setup_routes(self) -> None:
         # Import and register blueprints
-        from .auth import bp as auth_bp
-        self.register_blueprint(auth_bp)
+        from .routers.em_router import router as em_router
+        self.include_router(em_router)
+
+        from .routers.mc_router import router as mc_router
+        self.include_router(mc_router)
+
+        from .routers.ja_router import router as ja_router
+        self.include_router(ja_router)
+
+        # from .auth import bp as auth_bp
+        # self.register_blueprint(auth_bp)
         
-        from .routers import mc_recommendation_bp
-        self.register_blueprint(mc_recommendation_bp)
+        # from .routers import mc_recommendation_bp
+        # self.register_blueprint(mc_recommendation_bp)
         
-        from .routers import em_recommendation_bp
-        self.register_blueprint(em_recommendation_bp)
+        # from .routers import em_recommendation_bp
+        # self.register_blueprint(em_recommendation_bp)
         
-        from .routers import ja_recommendation_bp
-        self.register_blueprint(ja_recommendation_bp)
-        
-        @self.route('/health', methods=['GET'])
-        def health_check():
-            """
-            Health check endpoint for the API.
+        # from .routers import ja_recommendation_bp
+        # self.register_blueprint(ja_recommendation_bp)
 
-            Returns:
-                JSON: Health status of the API, showing 'healthy' if in deploy mode, or the current
-                operational mode otherwise.
-            """
-            return jsonify({
-                'status': self.mode if self.mode != 'deploy' else 'healthy',
-            })
-
-    def run(self, host: str = None, port: int = None, mode: str = 'deploy', load_dotenv: bool = True, sync: bool = False, **options) -> None:
-        """
-        Run the RecommendationAPI server with specified host, port, and mode settings.
-
-        Parameters:
-            host (str): The IP address to bind the server. Defaults to None.
-            port (int): The port on which the server will listen. Defaults to None.
-            mode (str): The mode in which to run the server, one of 'deploy', 'debug', or 'maintenance'.
-            load_dotenv (bool): If True, loads environment variables from .env. Defaults to True.
-            **options: Additional keyword arguments passed to Flask's run method.
-
-        Raises:
-            ValueError: If the specified mode is not one of 'deploy', 'debug', or 'maintenance'.
-
-        Returns:
-            None
-        """
-        ALL_MODE = ['deploy', 'debug', 'maintenance']
-        if sum(mode in m for m in ALL_MODE) != 1:
-            raise ValueError(f'The launching mode must be in : {ALL_MODE}')
-        self.mode = mode
-        self.limiter.init_app(self)
-        self.db.init_app(self)
-        self.jwt.init_app(self)
-        
-        if sync:
-            self.db.sync.sync_all(erase_data=True)
-
-        return super().run(host=host, port=port, debug=mode.lower() in 'debug', load_dotenv=load_dotenv, **options)
+app = RecommenderFastAPI()
